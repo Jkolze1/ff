6b84cf074a0219e61b5d2ecf06c2f4512bc09dcb
Jacqueline Kolze <38360663+Jkolze1@users.noreply.github.com>
2018-08-10 23:33:36 -0500

    Add files via upload

---
+++ friends.js
@@ -0,0 +1,91 @@
+// Cast of friends in the array
+var friendsArray = [
+	{
+		name: 'Monica',
+		photo: 'https://i.pinimg.com/originals/40/91/0e/40910e294fb74ecb1424c806cd584d92.jpg',
+		scores: [
+			'1',
+			'2',
+			'3',
+			'4',
+			'5',
+			'4',
+			'3',
+			'2',
+			'1',
+			'1'
+		]
+    },
+
+	{
+		name: 'Ross',
+		photo: 'https://vignette.wikia.nocookie.net/friends/images/0/0b/RossGeller.jpg/revision/latest?cb=20180424154547',
+		scores: [
+			'2',
+			'3',
+			'4',
+			'5',
+			'5',
+			'1',
+			'2',
+			'4',
+			'3',
+			'3'
+		]
+    },
+
+
+	{
+		name: 'Chandler',
+		photo: "https://vignette.wikia.nocookie.net/friends/images/2/21/2473459498_a3b4e40781.jpg/revision/latest?cb=20100601130820",
+		scores: [
+			'2',
+			'2',
+			'3',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4'
+		]
+    },
+
+	{
+		name: 'Phoebe',
+		photo: "https://upload.wikimedia.org/wikipedia/en/thumb/f/f6/Friendsphoebe.jpg/220px-Friendsphoebe.jpg",
+		scores: [
+			'5',
+			'4',
+			'3',
+			'2',
+			'1',
+			'1',
+			'2',
+			'3',
+			'4',
+			'5'
+		]
+    },
+
+   
+	{
+		name: 'Rachel',
+		photo: 'https://vignette.wikia.nocookie.net/loveinterest/images/9/93/Rachel_Green.png/revision/latest?cb=20160419025450',
+		scores: [
+			'4',
+			'3',
+			'2',
+			'5',
+			'5',
+			'4',
+			'3',
+			'4',
+			'1',
+			'5'
+		]
+	}
+];
+
+module.exports = friendsArray;
